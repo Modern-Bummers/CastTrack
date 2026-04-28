d186d488ba8b4b577e5f509543f1a80561aa689e
Thomas Ortiz <126148894+Modern-Bummers@users.noreply.github.com>
2026-04-28 04:53:35 -0700

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/navigationBar";
 import Footer from "./components/footer";
 import HomePage from "./pages/homePage";
 import RegulationPage from "./pages/regulationPage";
 import CatchPage from "./pages/catchPage";
 import EventPage from "./pages/eventPage";
-import LoginButton from "./components/login";
-
+import ReminderPage from "./pages/reminderPage";
 
 function App() {
   return (
@@ -18,6 +17,7 @@ function App() {
         <Route path="/regulationPage" element={<RegulationPage />} />
         <Route path="/catchPage" element={<CatchPage />} />
         <Route path="/eventPage" element={<EventPage />} />
+        <Route path="/reminders" element={<ReminderPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
